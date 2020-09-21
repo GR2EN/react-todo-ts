defda38d1de0f5fe250d62e516f1cbd9ced36d6e
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-09-21 16:27:58 +0600

    fix hot reload when public/db.json did update

--- src/store/todo/selectors.ts
+++ src/store/todo/selectors.ts
@@ -5,6 +5,11 @@ import { LoadingState, TodoState } from './contracts/state';
 
 export const selectTodos = (state: RootState): TodoState => state.todo;
 
-export const selectLoadingState = (state: RootState): LoadingState => selectTodos(state).loadingState;
+/* eslint implicit-arrow-linebreak: 0 */
+export const selectLoadingState = (state: RootState): LoadingState =>
+  selectTodos(state).loadingState;
 
-export const selectTodoItems = createSelector(selectTodos, (todo) => todo.items);
+export const selectTodoItems = createSelector(
+  selectTodos,
+  (todo) => todo.items
+);
