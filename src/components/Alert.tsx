394520c6c1711ebb81b832bad33462dbdc1462f3
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-09-23 20:36:48 +0600

    add alert, move icons

---
+++ src/components/Alert.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from 'react';
+
+import classNames from 'classnames';
+
+import ErrorIcon from './icons/ErrorIcon';
+import InfoIcon from './icons/InfoIcon';
+import SuccessIcon from './icons/SuccessIcon';
+
+export enum AlertType {
+  error = 'error',
+  info = 'info',
+  success = 'success',
+}
+
+export interface Alert {
+  severity?: AlertType;
+  children: string;
+}
+
+export const Alert: React.FC<Alert> = ({ children, severity }): ReactElement => (
+  <div className={classNames('alert', [severity && `alert--${severity}`])}>
+    <div className="alert__icon">
+      {severity === AlertType.error && <ErrorIcon />}
+      {severity === AlertType.info && <InfoIcon />}
+      {severity === AlertType.success && <SuccessIcon />}
+    </div>
+    <div className="alert__message">{children}</div>
+  </div>
+)
--- src/components/TodoAddField.tsx
+++ src/components/TodoAddField.tsx
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 
 import { fetchAddTodoItem } from '../store/todo/actionCreators';
 import { IconButton } from './IconButton';
-import { SvgIcon } from './SvgIcon';
+import AddIcon from './icons/AddIcon';
 
 export interface TodoAddField {
   placeholder: string;
@@ -42,9 +42,7 @@ export const TodoAddField: React.FC<TodoAddField> = ({
         value={value}
       />
       <IconButton color="primary" disabled={value.length === 0} onClick={handleSubmit}>
-        <SvgIcon>
-          <path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z" />
-        </SvgIcon>
+        <AddIcon />
       </IconButton>
     </div>
   );
