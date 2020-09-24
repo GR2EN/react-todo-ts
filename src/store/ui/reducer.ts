3f3f00423dc2ff273288566c0f1558459222916f
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-09-24 13:03:46 +0600

    add theme prefer

--- src/store/ui/reducer.ts
+++ src/store/ui/reducer.ts
@@ -1,13 +1,29 @@
 import { UiActions, UiActionTypes } from './actionCreators';
 import { UiMode, UiState } from './contracts/state';
 
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
+const currentTheme = localStorage.getItem('ui-mode');
+let initialUiMode;
+
+if (currentTheme) {
+  initialUiMode = currentTheme === UiMode.DARK ? UiMode.DARK : UiMode.LIGHT;
+} else if (prefersDarkMode.matches) {
+  initialUiMode = prefersDarkMode.matches ? UiMode.DARK : UiMode.LIGHT
+} else {
+  initialUiMode = UiMode.LIGHT;
+}
+
+localStorage.setItem('ui-mode', initialUiMode);
+
 const initialState: UiState = {
-  mode: UiMode.LIGHT,
+  mode: initialUiMode,
 }
 
 const ui = (state = initialState, action: UiActions): UiState => {
   switch (action.type) {
     case UiActionTypes.SET_UI_MODE: {
+      localStorage.setItem('ui-mode', action.payload);
+
       return {
         ...state,
         mode: action.payload,
@@ -15,9 +31,12 @@ const ui = (state = initialState, action: UiActions): UiState => {
     }
 
     case UiActionTypes.TOGGLE_UI_MODE: {
+      const nextUiMode = state.mode === UiMode.DARK ? UiMode.LIGHT : UiMode.DARK;
+      localStorage.setItem('ui-mode', nextUiMode);
+
       return {
         ...state,
-        mode: state.mode === UiMode.DARK ? UiMode.LIGHT : UiMode.DARK,
+        mode: nextUiMode,
       }
     }
 
